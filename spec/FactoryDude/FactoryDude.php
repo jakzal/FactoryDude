fe888833709b8079d50054746c308dfa51828fb7
Jakub Zalas <jakub@zalas.pl>
2012-09-09 16:53:29 +0100

    Injected world into the builder.

--- spec/FactoryDude/FactoryDude.php
+++ spec/FactoryDude/FactoryDude.php
@@ -16,6 +16,34 @@ class FactoryDude implements Specification
             return $builder;
         });
 
-        $this->factoryDude->offsetGet('User')->shouldBe($builder);
+        $this->factoryDude->offsetGet('User')->shouldReturn($builder);
+    }
+
+    /**
+     * @param ObjectStub $builder mock of FactoryDude\TestDataBuilder\TestDataBuilder
+     */
+    public function it_should_pass_the_world_to_the_builder($builder)
+    {
+        // @todo until PHPSpec supports this...
+        $builder = \Mockery::mock('FactoryDude\TestDataBuilder\TestDataBuilder');
+        $builder->shouldReceive('offsetSet')->once()->with('_world', $this->factoryDude->getStubSubject());
+
+        $this->factoryDude->offsetSet('User', function ($container) use ($builder) {
+            return $builder;
+        });
+
+        $this->factoryDude->offsetGet('User');
+    }
+
+    public function it_should_not_pass_the_world_to_the_non_builder()
+    {
+        $this->factoryDude->offsetSet('User', function ($container) {
+            $service = \Mockery::mock('stdClass');
+            $service->shouldReceive('offsetSet')->never();
+
+            return $service;
+        });
+
+        $this->factoryDude->offsetGet('User');
     }
 }
--- src/FactoryDude/FactoryDude.php
+++ src/FactoryDude/FactoryDude.php
@@ -4,4 +4,19 @@ namespace FactoryDude;
 
 class FactoryDude extends \Pimple
 {
+    /**
+     * @param string $id
+     *
+     * @return mixed
+     */
+    public function offsetGet($id)
+    {
+        $service = parent::offsetGet($id);
+
+        if ($service instanceof TestDataBuilder\TestDataBuilderInterface) {
+            $service['_world'] = $this;
+        }
+
+        return $service;
+    }
 }
